3999c33443cecfd6eefdb5401c5c1c2ef770737b
acaduck <58357364+acaduck@users.noreply.github.com>
2022-04-13 10:17:35 +0200

    Create cssvar2.js

---
+++ cssvar2.js
@@ -0,0 +1,31 @@
+/* global cssVars, css_beautify */
+
+cssVars({
+  // DEMO: Process only demo.css file
+  include: 'link[href*="project.css"]',
+  // DEMO: Treat modern browsers as legacy
+  onlyLegacy: false,
+  
+  // DEMO: Toggles to see results
+  // ----------------------------------------
+  // preserveStatic: false,
+  // preserveVars: true,
+  // updateURLs: false,
+  // variables: { '--color': 'purple' },
+  // ----------------------------------------
+  
+  onSuccess: function(cssText, elm, url) {
+    var srcElm = document.querySelector('#src');
+
+    srcElm.textContent += cssText;
+  },
+  onComplete: function(cssText, styleNodes, cssVariables, benchmark) {
+    var outElm = document.querySelector('#out');
+
+    // Format CSS (external library)
+    cssText = css_beautify(cssText);
+
+    // Update <code> tag with CSS result
+    outElm.textContent = cssText;
+  }
+})
